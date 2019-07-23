2e90c64e1ffb29001a8f8b1a94b6df988ea15140
VALLEY Antoine (T0173847) <antoine.valley@mythalesgroup.io>
2019-07-23 14:10:09 +0200

    Fix log

--- portainer_cli/__init__.py
+++ portainer_cli/__init__.py
@@ -229,7 +229,7 @@ class PortainerCLI(object):
         result = self.get_stacks()
         if result:
             for stack in result:
-                logger.debug('get_stack_by_name - Name={} - EndpointId={}'.format(stack['Name'], stack['EndpointId'])
+                logger.debug('get_stack_by_name - Name={} - EndpointId={}'.format(stack['Name'], stack['EndpointId']))
                 if stack['Name'] == stack_name and stack['EndpointId'] == endpoint_id:
                     return stack
         if mandatory:
